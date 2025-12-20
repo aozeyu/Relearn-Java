804f69f5f1415b88bf1b74f722c18b7dc340c316
aozeyu <3484766443@qq.com>
2025-12-20 20:00:57 +0800

    ifelse

---
+++ src/main/java/com/java/Ifelse.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class Ifelse {
+  public static void main(String[] args) {
+    int x = 8;
+    int y = 10;
+    String s = "something";
+    if (x  < y && s.length() > y) {
+      System.out.println("both true");
+    }else {
+      System.out.println("false");
+    }
+  }
+}
