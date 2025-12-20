55d21f8caee427aded2a77dc15171fef76f77584
aozeyu <3484766443@qq.com>
2025-12-20 20:08:20 +0800

    switch

---
+++ src/main/java/com/java/Swith.java
@@ -0,0 +1,21 @@
+package com.java;
+
+public class Swith {
+  public static void main(String[] args) {
+    int x = 3;
+    switch (x) {
+      case 1:
+        System.out.println("x is 1");
+        break;
+      case 2:
+        System.out.println("x is 2");
+        break;
+      case 3:
+        System.out.println("x is 3");
+        // break;
+      case 4:
+        System.out.println("x is 4");
+        break;
+    }
+  }
+}
