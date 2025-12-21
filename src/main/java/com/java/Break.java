ef7e5056ab8f9dc270430a839167b49b26545998
aozeyu <3484766443@qq.com>
2025-12-21 10:41:27 +0800

    break 2

--- src/main/java/com/java/Break.java
+++ src/main/java/com/java/Break.java
@@ -3,15 +3,22 @@ package com.java;
 public class Break {
   public static void main(String[] args) {
     int i = 0;
+    OUTER:
     while (true) {
       i++;
-      if (i % 2 == 0) {
-        continue;
-      }
-      System.out.println("Hi " + i);
-      if (i > 100) {
-        break;
+      INNER:
+      while (true) {
+        int y = 0;
+
+        if (i % 2 == 0) {
+          continue INNER;
+        }
+        System.out.println("Hi " + i);
+        if (i > 100) {
+          break OUTER;
+        }
       }
     }
+
   }
 }
