ca4ceda806a08270785b3195bc5a26028fb813bb
aozeyu <3484766443@qq.com>
2025-12-21 09:47:18 +0800

    switch

---
+++ src/main/java/com/java/Loop.java
@@ -0,0 +1,25 @@
+package com.java;
+
+public class Loop {
+  public static void main(String[] args) {
+//    for (int i = 0; i < 10; i++) {
+//      System.out.println(i + " it");
+//    }
+
+//    String[] strings = {"hi","hello"};
+//    for (String s : strings) {
+//      System.out.println(s);
+//    }
+//      int i = 0;
+//      while (i < 15) {
+//        System.out.println(i);
+//        i++;
+//      }
+
+    int i =0;
+    do {
+      System.out.println(i);
+      i++;
+    } while (i<0);
+  }
+}
